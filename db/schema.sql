71738be9c6bb002a0e70d0ac308be9eeb5dab13f
William Leung <leungwkalt@gmail.com>
2017-05-25 10:31:22 -0700

    mvc created

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Overwatch;
+USE Overwatch;
+
+Create table Maps(
+  --three maps, user_hero0, hero1, hero2, hero3, hero4, hero5
+)
+
+create table user_registry(
+  --username and password encrypted
+)
---
+++ routes/api-routes.js
@@ -0,0 +1,11 @@
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+//CRUD
+};
