828991a04c90cce32fc56575d62d78df4e6ca19f
William Leung <leungwkalt@gmail.com>
2017-05-26 12:14:53 -0700

    user-entry hero panel

---
+++ public/js/user-entry.js
@@ -0,0 +1,12 @@
+
+  //
+  //   $(document).on("click", "#map-select", function() {
+  //
+  //       event.preventDefault();
+  //       $(this).addClass("active");
+  //       $("#reddit-btn").removeClass("active")
+  //       $("#youtube-btn").removeClass("active");
+  //   })
+  $(".hero-portrait-image").click(function () {
+     $(this).toggleClass("orange");
+  });
