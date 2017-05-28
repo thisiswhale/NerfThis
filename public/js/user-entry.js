856bb0f1606937b7a21aa9e59f7cff6111c1f085
William Leung <leungwkalt@gmail.com>
2017-05-27 23:48:05 -0700

    added comments

--- public/js/user-entry.js
+++ public/js/user-entry.js
@@ -1,3 +1,4 @@
+//enables to change hero's portrait background color
 $(".hero-portrait-image").click(function() {
   event.preventDefault();
   var limit = 6;
@@ -16,10 +17,13 @@ $(".hero-portrait-image").click(function() {
   // console.log(teamComp);
 });
 
+//dynamically append the map arena based by map types from btn group
+var mapType;
 $(document.body).on("click", ".click-map-select", function(event) {
   event.preventDefault();
-  //  console.log($("#map-select > label.focus").val());
+
   console.log($(this).children()[0].value);
+  mapType = $(this).children()[0].value;
   $("#dropdown-map").empty();
   switch ($(this).children()[0].value) {
     case "assault":
@@ -47,33 +51,41 @@ $(document.body).on("click", ".click-map-select", function(event) {
   }
 });
 
+//get map value
+var mapSelect;
 $(document.body).on("click", "#dropdown-map li a", function(){
   event.preventDefault();
   $(this).parents(".btn-group").find('.selection').text($(this).text());
 
   //this gets the value attr when selected
   console.log($(this).attr("value"));
+  mapSelect = $(this).attr("value");
 });
 
+//get attack/defend value
+var position;
 $(document.body).on("click", "#map-select-position .btn", function(){
   event.preventDefault();
 
   //this gets the value attr when selected
   console.log($(this).find("input").val());
+  position = $(this).find("input").val();
 });
 
+//get win/lose value
+var result;
 $(document.body).on("click", "#map-select-result .btn", function(){
   event.preventDefault();
 
   //this gets the value attr when selected
   console.log($(this).find("input").val());
+  result = $(this).find("input").val();
 });
 
 //The FINAL ENTRY ROUND SUBMISSION
 //needs to add the data from win/loss, atk/defend, map type, map area
 $('#entry-submit').on('click', function(event){
     event.preventDefault();
-
     //gets team comp
     var teamComp=[];
     $('.hero-portrait-image.orange').each(function(){
@@ -81,5 +93,5 @@ $('#entry-submit').on('click', function(event){
     });
     console.log(teamComp);
 
-    
+
 });
