a1544964d4bd25c7e2ca3598e650d8d260ff69f4
Yuhang Chen <hangyuchen@msn.com>
2017-05-27 23:09:19 -0700

    hah

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var passport = require('passport');
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+var router = express.Router();
+
+module.exports = function(app){
+
+router.get('/', ensureLoggedIn, function(req, res, next) {
+  res.render('user', { user: req.user });
+});
+
+}
--- server.js
+++ server.js
@@ -4,19 +4,91 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
+var express = require("express"),app = express();
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-
+var path = require('path');
+var logger = require('morgan');
+var session = require('express-session');
+var dotenv = require('dotenv');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
 // Sets up the Express App
 // =============================================================
-var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
+dotenv.load();
 
-// Sets up the Express app to handle data parsing
+var db = require("./models");
+var user = require('./routes/user')(app);
+var routes = require("./routes/html-routes.js")(app);
+var apiRoutes =require("./routes/api-routes.js")(app);
+// Requiring our models for syncing
+// app.use('/user', user);
+
+var strategy = new Auth0Strategy({
+    domain:       process.env.AUTH0_DOMAIN,
+    clientID:     process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  }, function(accessToken, refreshToken, extraParams, profile, done) {
+    // Store the profile and tokens in the user object.
+    return done(null, {
+      profile: profile,
+      extraParams: extraParams,
+    });
+  });
+
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'shhhhhhhhh',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'views')));
+// app.use(app.router)(app);
+// routes.initialize(app);
+
+
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
@@ -27,8 +99,7 @@ app.use(express.static(process.cwd() +"/public"));
 app.use(methodOverride("_method"));
 
 // Routes =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync().then(function() {
@@ -36,3 +107,8 @@ db.sequelize.sync().then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+module.exports = app;
+
+
+
