daec4794b7cd5a56128477526a91ad1782218b45
William Leung <leungwkalt@gmail.com>
2017-05-27 22:34:11 -0700

    working hero select, map select, map type select, win/loss select, position select

--- public/js/user-entry.js
+++ public/js/user-entry.js
@@ -1,21 +1,19 @@
-//
-//   $(document).on("click", "#map-select", function() {
-//
-//       event.preventDefault();
-//       $(this).addClass("active");
-//       $("#reddit-btn").removeClass("active")
-//       $("#youtube-btn").removeClass("active");
-//   })
-
 $(".hero-portrait-image").click(function() {
   event.preventDefault();
   var limit = 6;
+
   if ($(".hero-portrait-image.orange").length < limit) {
     $(this).toggleClass("orange");
   } else {
     $(this).css("background-color", "grey");
   }
-
+  //use this beneath to test out for array of adding team
+  // var teamComp=[];
+  // $('.hero-portrait-image.orange').each(function(){
+  //     teamComp.push($(this).attr("name"));
+  //
+  // });
+  // console.log(teamComp);
 });
 
 $(document.body).on("click", ".click-map-select", function(event) {
@@ -25,7 +23,7 @@ $(document.body).on("click", ".click-map-select", function(event) {
   $("#dropdown-map").empty();
   switch ($(this).children()[0].value) {
     case "assault":
-      $("#dropdown-map").append("<li><a href='#' value='hanamura'>hanamura</a></li>");
+      $("#dropdown-map").append("<li><a href='#' value='hanamura'>Hanamura</a></li>");
       $("#dropdown-map").append("<li><a href='#' value='temple-of-anubis'>Temple of Anubis</a></li>");
       $("#dropdown-map").append("<li><a href='#' value='voskaya-industries'>Voskaya Industries</a></li>");
       break;
@@ -44,8 +42,44 @@ $(document.body).on("click", ".click-map-select", function(event) {
       $("#dropdown-map").append("<li><a href='#' value='illios'>Illios</a></li>");
       $("#dropdown-map").append("<li><a href='#' value='li-jiang-tower'>Li-jiang Tower</a></li>");
       $("#dropdown-map").append("<li><a href='#' value='nepal'>Nepal</a></li>");
-      $("#dropdown-map").append("<li><a href='#' value='oasis'>Oasis</a></li>");    
+      $("#dropdown-map").append("<li><a href='#' value='oasis'>Oasis</a></li>");
       break;
   }
+});
 
+$(document.body).on("click", "#dropdown-map li a", function(){
+  event.preventDefault();
+  $(this).parents(".btn-group").find('.selection').text($(this).text());
+
+  //this gets the value attr when selected
+  console.log($(this).attr("value"));
+});
+
+$(document.body).on("click", "#map-select-position .btn", function(){
+  event.preventDefault();
+
+  //this gets the value attr when selected
+  console.log($(this).find("input").val());
+});
+
+$(document.body).on("click", "#map-select-result .btn", function(){
+  event.preventDefault();
+
+  //this gets the value attr when selected
+  console.log($(this).find("input").val());
+});
+
+//The FINAL ENTRY ROUND SUBMISSION
+//needs to add the data from win/loss, atk/defend, map type, map area
+$('#entry-submit').on('click', function(event){
+    event.preventDefault();
+
+    //gets team comp
+    var teamComp=[];
+    $('.hero-portrait-image.orange').each(function(){
+        teamComp.push($(this).attr("name"));
+    });
+    console.log(teamComp);
+
+    
 });
