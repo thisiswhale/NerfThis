aef609496ad2518770ec46b50ea64b03bf7d4c9f
Eric <eric@someone.com>
2017-05-30 11:37:44 -0700

    cleaned up server js

--- server.js
+++ server.js
@@ -4,51 +4,24 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express"),app = express();
+var express = require("express");
+var app = express();
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require('path');
-var logger = require('morgan');
-var session = require('express-session');
-var dotenv = require('dotenv');
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
+
+// var session = require('express-session');
+
 // Sets up the Express App
 // =============================================================
 var PORT = process.env.PORT || 8080;
 
-dotenv.load();
-
 var db = require("./models");
 var user = require('./routes/user')(app);
 var routes = require("./routes/html-routes.js")(app);
-var apiRoutes =require("./routes/api-routes.js")(app);
+var apiRoutes = require("./routes/api-routes.js")(app);
 // Requiring our models for syncing
 // app.use('/user', user);
-
-var strategy = new Auth0Strategy({
-    domain:       process.env.AUTH0_DOMAIN,
-    clientID:     process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-  }, function(accessToken, refreshToken, extraParams, profile, done) {
-    // Store the profile and tokens in the user object.
-    return done(null, {
-      profile: profile,
-      extraParams: extraParams,
-    });
-  });
-
-passport.use(strategy);
-
-// you can use this section to keep a smaller payload
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
@@ -58,9 +31,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
-  secret: 'shhhhhhhhh',
-  resave: true,
-  saveUninitialized: true
+    secret: 'shhhhhhhhh',
+    resave: true,
+    saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -95,7 +68,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
-app.use(express.static(process.cwd() +"/public"));
+app.use(express.static(process.cwd() + "/public"));
 app.use(methodOverride("_method"));
 
 // Routes =============================================================
@@ -103,12 +76,9 @@ app.use(methodOverride("_method"));
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });
 
 module.exports = app;
-
-
-
