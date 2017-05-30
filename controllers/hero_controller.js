f6b246c95c465744c6471546509e23c310359c8f
KathrinDavis <kathrinjdavis@gmail.com>
2017-05-30 13:47:46 -0700

    ta

--- controllers/hero_controller.js
+++ controllers/hero_controller.js
@@ -2,3 +2,10 @@ var express = require("express");
 
 var router = express.Router();
 var hero = require("../models/hero.js");
+module.exports = {
+  findAll: function(req, res, next) {
+    hero.findAll().then(function(data) {
+      res.json(data);
+    })
+  }
+}
