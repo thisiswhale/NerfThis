e13bbbfdd69a238e1e87fdf62db35047bcb3f292
KathrinDavis <kathrinjdavis@gmail.com>
2017-05-30 13:18:53 -0700

    map objects

--- models/map.js
+++ models/map.js
@@ -1,9 +1,9 @@
 module.exports= function(sequelize, DataType){
 	var Map = sequelize.define("Map", {
-		map: {
-			type: DataTypes.STRING,
-			name: DataTypes.STRING	
-		}
+	
+			map_type: {type: DataTypes.STRING},
+			name: {type: DataTypes.STRING}	
+
 	},
 	    {
       // ROUTES We're saying that we want our Round to have Map
