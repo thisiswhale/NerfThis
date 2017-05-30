ae415a11612a2fdbee02d256943843ab1ed58553
KathrinDavis <kathrinjdavis@gmail.com>
2017-05-30 13:48:59 -0700

    login controller added

---
+++ controllers/login_controller.js
@@ -0,0 +1,30 @@
+var bcrypt = require('bcryptjs');
+var db = require('./../models');
+
+module.exports = {
+  saveUser: function(req, res) {
+    var body = req.body;
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(body.password, salt, function(err, hash) {
+        // Store hash in your password DB.
+        db.User.create({
+          name: req.body.name,
+          password: hash
+        }).then(function(user) {
+          res.json(user);
+        })
+      });
+    });
+  },
+  authenticateUser: function(req, res) {
+    //query for password where username = req.body.username
+    bcrypt.compare(req.body.password, queryResults, function(err, res) {
+      if (res) {
+        res.json(true)
+      } else {
+        res.json(false)
+      }
+    })
+  }
+}
+
