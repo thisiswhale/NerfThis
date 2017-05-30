7f05456713e7faf2fb2e72237c4aae53f36ebe15
KathrinDavis <kathrinjdavis@gmail.com>
2017-05-30 00:03:23 -0700

    some api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,4 +8,26 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 //CRUD
+  app.get("/api/", function(rec,res){
+  var query = {};
+  if (req.query.user_id;){
+    query.UserId= req.query.user_id;
+  }
+    //to find round history for player
+  db.Round.findAll({
+    where: query,
+    include:[db.User]
+  }).then(fucntion(dbRound){
+          res.json(dbRound);
+  });
+});
+  
+app.get("/api/hero/:id", function(req,res){
+db.Hero.findOne({
+  where: {idL req.params.id},
+   include: [db.User]}).then(function(dbHero) {
+    res.json(dbHero);
+  });
+});
+
 };
