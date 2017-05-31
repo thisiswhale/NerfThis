2edced7d2d0ab9fb2ac74f11715346d6ef1001af
Eric <eric@someone.com>
2017-05-30 23:22:46 -0700

    updated html route and picker links

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,15 +16,18 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-    app.get("/dashboard", function(req, res){
+    app.get("/dashboard", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/dashboard.html"));
     });
-    app.get("/entry", function(req, res){
+    app.get("/entry", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/user-entry.html"));
     });
-//     app.get("/about", function(req, res){
-//         res.sendFile(path.join(__dirname, "../public/about.html"));
-//     });
+    app.get("/picker", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/pick.html"));
+    });
+    //     app.get("/about", function(req, res){
+    //         res.sendFile(path.join(__dirname, "../public/about.html"));
+    //     });
 
 
     app.get('/login',
