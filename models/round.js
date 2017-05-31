40f8c07e3eab0a95bf7de0c67617f8681a2ec056
KathrinDavis <kathrinjdavis@gmail.com>
2017-05-30 23:49:00 -0700

    defaultValue

--- models/round.js
+++ models/round.js
@@ -1,49 +1,50 @@
 module.exports= function(sequelize, DataTypes){
 	var Round = sequelize.define("Round", {
 			
-			Genji: {type:DataTypes.BOOLEAN, default: false},
-			McCree: {type:DataTypes.BOOLEAN, default: false},
-			Pharah: {type:DataTypes.BOOLEAN, default: false},
-			Reaper: {type:DataTypes.BOOLEAN, default: false},
-			Soldier76: {type:DataTypes.BOOLEAN, default: false},
-			Sombra: {type:DataTypes.BOOLEAN, default: false},
-			Tracer: {type:DataTypes.BOOLEAN, default: false},
-			Bastion: {type:DataTypes.BOOLEAN, default: false},
-			Hanzo: {type:DataTypes.BOOLEAN, default: false},
-			Junkrat: {type:DataTypes.BOOLEAN, default: false},
-			Mei: {type:DataTypes.BOOLEAN, default: false},
-			Torbjorn: {type:DataTypes.BOOLEAN, default: false},
-			Widowmaker: {type:DataTypes.BOOLEAN, default: false},
-			DVa: {type:DataTypes.BOOLEAN, default: false},
-			Orisa: {type:DataTypes.BOOLEAN, default: false},
-			Reinhardt: {type:DataTypes.BOOLEAN, default: false},
-			Roadhog: {type:DataTypes.BOOLEAN, default: false},
-			Winston: {type:DataTypes.BOOLEAN, default: false},
-			Zarya: {type:DataTypes.BOOLEAN, default: false},
-			Ana: {type:DataTypes.BOOLEAN, default: false},
-			Lucio: {type:DataTypes.BOOLEAN, default: false},
-			Mercy: {type:DataTypes.BOOLEAN, default: false},
-			Symmetra: {type:DataTypes.BOOLEAN, default: false},
-			Zenyatta: {type:DataTypes.BOOLEAN, default: false},
+			Genji: {type:DataTypes.BOOLEAN, defaultValue: false},
+			McCree: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Pharah: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Reaper: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Soldier76: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Sombra: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Tracer: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Bastion: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Hanzo: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Junkrat: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Mei: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Torbjorn: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Widowmaker: {type:DataTypes.BOOLEAN, defaultValue: false},
+			DVa: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Orisa: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Reinhardt: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Roadhog: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Winston: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Zarya: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Ana: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Lucio: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Mercy: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Symmetra: {type:DataTypes.BOOLEAN, defaultValue: false},
+			Zenyatta: {type:DataTypes.BOOLEAN, defaultValue: false},
 
 			mapType: {type: DataTypes.STRING},
-      		mapName: {type: DataTypes.STRING},
-			attack: {type:DataTypes.BOOLEAN, default: true},
-			victory:  {type:DataTypes.BOOLEAN, default: true}
+      			mapName: {type: DataTypes.STRING},
+		
+			attack: {type:DataTypes.BOOLEAN, defaultValue: true},
+			victory:  {type:DataTypes.BOOLEAN, defaultValue: true}
 		},
 		{
       // We're saying that we want our User to have Round
-      classMethods: {
-        associate: function(models) {
+    	classMethods: {
+        	associate: function(models) {
           // An User (foreignKey) is required or a Round can't be made
-          Round.belongsTo(models.User, {
-            foreignKey: {
-              allowNull: false
-            }
-          });
-        }
-      }
-    }
+          	Round.belongsTo(models.User, {
+            		foreignKey: {
+              			allowNull: false
+            		}
+          	});
+        	}
+      	}
+    		}
    );
 	return Round;
 };
